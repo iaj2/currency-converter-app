f47abd3a5b5851347551339f7024a36039a677b6
iaj2 <isaacjames124@yahoo.ca>
2023-07-04 06:15:07 -0700

    add basic ui boiler code.

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,25 @@
+import {React, useState} from 'react';
+import DropdownMenu from './dropdown-menu'
+import {ReactComponent as CanadaFlagIcon} from './icons/canada-flag-icon.svg';
+import {ReactComponent as GBFlagIcon} from './icons/united-kingdom-flag-icon.svg';
 
 function App() {
+  const [triggerIconFrom, setTriggerIconFrom] = useState(<CanadaFlagIcon />);
+  const [selectedFrom, setSelectedFrom] = useState('CAD');
+  const [triggerIconTo, setTriggerTo] = useState(<GBFlagIcon />);
+  const [selectedTo, setSelectedTo] = useState('GBP');
+
   return (
-  
+  <div className='App'>
+    <h1 className='app-title'>Currency Converter</h1>
+    <h3 className='app-subtitle'>Check live foreign currency exchange rates</h3>
+    <div className='conversion-container'>
+      <DropdownMenu className="from-currency" triggerIcon={triggerIconFrom} selectedItem={selectedFrom}/>
+      <DropdownMenu className="to-currency" triggerIcon={triggerIconTo} selectedItem={selectedTo}/>
+    </div>
+  </div>
   );
 }
+
 
 export default App;
