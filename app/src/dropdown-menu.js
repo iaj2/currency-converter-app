d49b601165707dbbd7c57f4176e518b03d6d8c97
iaj2 <isaacjames124@yahoo.ca>
2023-07-10 09:40:37 -0700

    add rest of currencies

--- app/src/dropdown-menu.js
+++ app/src/dropdown-menu.js
@@ -1,6 +1,17 @@
 import React, {useState} from 'react';
 import {ReactComponent as CanadaFlagIcon} from './icons/canada-flag-icon.svg';
+import { ReactComponent as USFlagIcon } from './icons/usa-flag-icon.svg';
 import {ReactComponent as UKFlagIcon} from './icons/uk-flag-icon.svg';
+import {ReactComponent as EUFlagIcon} from './icons/eu-flag-icon.svg';
+import {ReactComponent as AUFlagIcon} from './icons/australia-flag-icon.svg';
+import {ReactComponent as JPFlagIcon} from './icons/japan-flag-icon.svg';
+import {ReactComponent as SwissFlagIcon} from './icons/swiss-flag-icon.svg';
+import {ReactComponent as NZFlagIcon} from './icons/new-zealand-flag-icon.svg';
+import {ReactComponent as RUSFlagIcon} from './icons/russia-flag-icon.svg';
+import {ReactComponent as SAFlagIcon} from './icons/south-africa-flag-icon.svg';
+import {ReactComponent as MXFlagIcon} from './icons/mexico-flag-icon.svg';
+import {ReactComponent as UAEFlagIcon} from './icons/uae-flag-icon.svg';
+
 
 
 function DropdownMenu({ className, triggerIcon, setTriggerIcon, selected, setSelected}) {
@@ -14,11 +25,17 @@ function DropdownMenu({ className, triggerIcon, setTriggerIcon, selected, setSel
   
     const dropdownItems = [
       { icon: <CanadaFlagIcon />, name: "CAD" },
+      { icon: <USFlagIcon />, name: "USD"},
+      { icon: <EUFlagIcon />, name: "EUR" },
       { icon: <UKFlagIcon />, name: "GBP" },
-      { icon: <CanadaFlagIcon />, name: "CAD" },
-      { icon: <UKFlagIcon />, name: "GBP" },
-      { icon: <CanadaFlagIcon />, name: "CAD" },
-      { icon: <UKFlagIcon />, name: "GBP" }
+      { icon: <AUFlagIcon />, name: "AUD" },
+      { icon: <JPFlagIcon />, name: "JPY"},
+      { icon: <SwissFlagIcon />, name: "CHF"},
+      { icon: <NZFlagIcon />, name: "NZD"},
+      { icon: <RUSFlagIcon />, name: "RUB"},
+      { icon: <SAFlagIcon />, name: "ZAR"},
+      { icon: <MXFlagIcon />, name: "MXN"},
+      { icon: <UAEFlagIcon />, name: "UAE"}
     ];
 
     return (
@@ -47,8 +64,18 @@ function DropdownMenu({ className, triggerIcon, setTriggerIcon, selected, setSel
 
 function DropdownItem({icon, name, onClick}){
   const nameMap = {
-    'CAD': 'CAD - Canadian Dollars',
-    'GBP': 'GBP - British Pounds'
+    'CAD': 'CAD - Canadian Dollar',
+    'GBP': 'GBP - British Pound',
+    'EUR': 'EUR - Euros',
+    'AUD': 'AUD - Australian Dollar',
+    'JPY': 'JPY - Japanese Yen',
+    'CHF': 'CHF - Swiss Franc',
+    'NZD': 'NZD - New Zealand Dollar',
+    'RUB': 'RUB - Russian Rouble',
+    'ZAR': 'ZAR - South African Rand',
+    'MXN': 'MXN - Mexican Peso',
+    'UAE': 'UAE - United Arab Emirates Dirham',
+    'USD': 'USD - US Dollar'
   }
 
   return(
