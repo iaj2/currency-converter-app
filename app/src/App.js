7008a5fd5047ad6855657b01e1418454c340c345
iaj2 <isaacjames124@yahoo.ca>
2023-08-01 18:46:36 -0700

    api function

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import DropdownMenu from './dropdown-menu'
 import {ReactComponent as CanadaFlagIcon} from './icons/canada-flag-icon.svg'
 import {ReactComponent as UKFlagIcon} from './icons/uk-flag-icon.svg';
 
 function App() {
+  const [exchangeData, setExchangeData] = useState(null);
   const [triggerIconFrom, setTriggerIconFrom] = useState(<CanadaFlagIcon />);
   const [selectedFrom, setSelectedFrom] = useState('CAD');
   const [triggerIconTo, setTriggerIconTo] = useState(<UKFlagIcon />);
   const [selectedTo, setSelectedTo] = useState('GBP');
+
+  useEffect(() => {
+    const fectchExchangeData = async () => {
+      try{
+        const response = await fetch("http://localhost:5000");
+        const data = response.json();
+        setExchangeData(data);
+      } catch (error) {
+        console.error('Error fetching exchange data: ', error);
+      }
+    }
+
+    fectchExchangeData();
+  }, [])
 
   return (
   <div className='App'>
